98807f694a35110c3a2282382500828c5531cb38
M7amed <161085169+M7amed121@users.noreply.github.com>
2024-05-25 19:06:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ navItems.forEach(function(navItem) {
         navBar.style.display = "none";
     });
 });
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const createSnowflake = () => {
+        const snowflake = document.createElement('div');
+        snowflake.classList.add('snowflake');
+        snowflake.textContent = '❆';
+        snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+        snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`; // تقليل كمية الثلج عبر زيادة المدة الزمنية
+        snowflake.style.fontSize = `${Math.random() * 10 + 10}px`;
+        document.body.appendChild(snowflake);
+
+        setTimeout(() => {
+            snowflake.remove();
+        }, 10000); // مدة بقاء الثلج
+    }
+
+    setInterval(createSnowflake, 2500); // تقليل التكرار لخلق ثلج أقل
+});
